a1885326becf656e0dfb09bc19c0d78688cade3c
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-09-24 15:27:25 +0530

    Update settings.py

--- bot/plugins/settings.py
+++ bot/plugins/settings.py
@@ -65,7 +65,7 @@ async def settings(bot, update):
         [
             InlineKeyboardButton
                 (
-                    "Close 🔐", callback_data="close"
+                    "Close 🤧", callback_data="close"
                 )
         ]
     ]
